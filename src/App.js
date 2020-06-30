8bd43dbc1874f7acd58bc4648ddb630c02bbb351
seif <seif@seif.rocks>
2020-06-30 03:09:33 +0300

    fixed footer message

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 function App() {
   return (
-
-      <Pane
-        height='100%'
-        width='100%'
-        background='#BCD0E7'
-        display="flex"
-        // padding={26}
-        // margin={26}
-        alignItems="center"
-        justifyContent="center"
-      >
-        <NavBar />
-        <Timer />
-      </Pane>
+    <Pane
+      height='100%'
+      width='100%'
+      background='#BCD0E7'
+      display="flex"
+      // padding={26}
+      // margin={26}
+      alignItems="center"
+      justifyContent="center"
+    >
+      <NavBar />
+      <Timer />
+    </Pane>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+
     <App />
     <section className="footer1 cid-qEsj1x8oQy" id="footer1-c">
       <div className="container">
         <div className="mbr-col-sm-12 align-center mbr-white">
           <p className="mbr-text mbr-fonts-style display-7" style={{ fontSize: '12px', lineHeight: '20px' }}> Built with
-						<span>❤️</span> by <a target="_blank" rel="noopener noreferrer" href="https://seif.rocks">Seif Elmughrabi</a> using AMP and hosted on Google Cloud</p>
+						<span>❤️</span> by <a target="_blank" rel="noopener noreferrer" href="https://seif.rocks">Seif Elmughrabi</a> using react and netlify</p>
         </div>
 
       </div>
